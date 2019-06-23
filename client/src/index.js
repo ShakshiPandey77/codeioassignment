d2836703b6985337e85604ee5d882c4ff61c9118
Shehyaaz <49763208+Shehyaaz@users.noreply.github.com>
2019-06-23 22:35:02 +0530

    Add files via upload
    Added BrowserRouter and Route tags to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import App from './App';
+import Subject from './components/subject/Subject';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+		<Router>
+			<div>
+				<Route path="/" component={App} /> {/*this path is not exact so that App is always rendered*/}
+				<Route path="/subject" exact component={Subject} />
+				{/* <Route path="/course" exact component={Course} />
+				<Route path="/topic" exact component={Topic} /> */}
+			</div>
+		</Router>
+	);
+ReactDOM.render(routing,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
